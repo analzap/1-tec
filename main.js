4d6a0a15246f7135b298f9a4af67560479789de3
analzap <163419955+analzap@users.noreply.github.com>
2024-11-07 09:57:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let numero1 =parseInt( prompt("informe o numero 1: "));
+let numero2 = parseInt(prompt("informe o numero 2: "));
+let soma = numero1 + numero2;
+document.write(" a soma e igual a: " , soma);
+console.log("o valor da soma no console e igual a: ", soma);
